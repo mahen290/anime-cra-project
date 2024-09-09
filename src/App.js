b99431e51150be0f560f742b4b16bea22d0119c5
mahen290 <mahendraxx@gmail.com>
2024-09-09 01:25:01 -0700

    MY First CRA Project Anime Has Build Today

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> Most Welcome </h1>
+      <header> Anime Project With Create React App </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Popular.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Popular() 
+{
+  return (
+    <div>
+      <h2> Popular </h2>
+    </div>
+  )
+}
+
+export default Popular;
